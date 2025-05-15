f58481d74c5b510e7174fd7e9cb31b4594111208
Mori <145260120+Mori-ii@users.noreply.github.com>
2025-05-15 18:14:25 +0800

    Update OneClickModel.cs

--- frontend/Models/OneClickModel.cs
+++ frontend/Models/OneClickModel.cs
@@ -6,13 +6,6 @@ namespace frontend.Models
 {
     public class OneClickModel
     {
-        [DllImport("user32.dll")]
-        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
-
-        private const uint WM_SYSCOMMAND = 0x0112;
-        private const uint SC_MONITORPOWER = 0xF170;
-        private const uint SC_SCREENSAVE = 0xF140;
-
         public void ExecuteShutdown()
         {
             Process.Start(new ProcessStartInfo("shutdown", "/s /t 0"));
@@ -26,9 +19,8 @@ namespace frontend.Models
         [DllImport("powrprof.dll", SetLastError = true)]
         private static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
 
-        public void ExecuteSleep(IntPtr windowHandle)
+        public void ExecuteSleep()
         {
-            // 使用系统休眠API替代消息发送
             SetSuspendState(false, false, false);
         }
     }
