63269d5ad9188b74352ba4d716114dc1018a1797
qianshuigeee <14630887+qianshuigeee@user.noreply.gitee.com>
2025-05-28 23:02:00 +0800

    add CountdownDay

---
+++ frontend/Models/CountdownDay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace frontend.Models
+{
+    using System;
+
+    public class CountdownDay
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime TargetDate { get; set; }
+        public int DaysDifference { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                if (DaysDifference == 0)
+                    return $"{Name}: 就是今天！";
+                else if (DaysDifference > 0)
+                    return $"{Name}: {DaysDifference}天后是{Name}";
+                else
+                    return $"{Name}: 已经过去{-DaysDifference}天";
+            }
+        }
+    }
+
+}
--- frontend/obj/Debug/net8.0-windows/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net8.0-windows/frontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ebaa9e724a722378462e8e52582dc51ebe3fb46")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45b3aa4f6b6a78d566f34207db8310ce97c64e03")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
